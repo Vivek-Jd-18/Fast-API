2082d308d18cf0f1e3e32909dda117230d0448e4
Vivek-Jd-18 <sergiokunaguero0009@gmail.com>
2024-07-10 09:26:49 +0530

    fix

--- app/db.py
+++ app/db.py
@@ -1,6 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
 
-cred = credentials.Certificate(r"C:\Users\admin\Desktop\Learnings\FastAPI\aviato-task\credentials\aviato-01-firebase-adminsdk-99aa1-ee8ba4ad8b.json")
+
+# cred = credentials.Certificate(r"C:\Users\admin\Desktop\Learnings\FastAPI\git-project\Fast-API\app\credentials\aviato-02-firebase-adminsdk-83j9s-ed26195547.json")
+
+# default_app = firebase_admin.initialize_app(cred)
+# db = firestore.client()
+
+
+relative_path = os.path.join(os.getcwd(), 'app', 'credentials', 'aviato-02-firebase-adminsdk-83j9s-ed26195547.json')
+
+cred = credentials.Certificate(relative_path)
 default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
