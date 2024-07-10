2e2b235cdee6d2320a95c8fb31054f5a57c78ec4
Vivek-Jd-18 <sergiokunaguero0009@gmail.com>
2024-07-10 10:00:41 +0530

    fix

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
-from . import firebase_creds
+from .etc.secrets import firebase_creds
 
 cred = credentials.Certificate(firebase_creds.firebase_credentials)
 default_app = firebase_admin.initialize_app(cred)
